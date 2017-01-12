f43a18db4e5c8de8d644b5b10e4e5f29af20ec8d
bobidio <bobidio@gmail.com>
2017-01-12 18:09:13 +0200

    add todo model and post/todo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-var express= require('express');
-var bodyParser= require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 var _ = require('underscore');
+
+var db = require('./db.js');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -9,24 +11,28 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 
-app.get('/', function (req, res){
+app.get('/', function(req, res) {
 	res.send('todo api root');
 });
 
 
 
-app.get('/todos', function (req, res) {
+app.get('/todos', function(req, res) {
 	var queryParams = req.query;
 	var filteredTodos = todos;
 
 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
+		filteredTodos = _.where(filteredTodos, {
+			completed: true
+		});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
+		filteredTodos = _.where(filteredTodos, {
+			completed: false
+		});
 	}
 
 	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function (todo) {
+		filteredTodos = _.filter(filteredTodos, function(todo) {
 			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
 		});
 	}
@@ -34,11 +40,13 @@ app.get('/todos', function (req, res) {
 	res.json(filteredTodos);
 });
 
-app.get('/todos/:id', function(req, res){
+app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
-	
-	if (matchedTodo){
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
+
+	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
@@ -48,43 +56,57 @@ app.get('/todos/:id', function(req, res){
 });
 
 
-app.post('/todos', function (req, res){
+app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
+	db.todo.create(body).then(function (todo){
+		res.json(todo.toJSON());
 
-	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
-		return res.status(400).send();
-	}
+	}, function (e) {
+		res.status(400).json(e);
 
-	body.description = body.description.trim();
-
-
-	body.id =todoNextId++;
-
-	todos.push(body);
+	});
 
 
+	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+	// 	return res.status(400).send();
+	// }
 
-	res.json(body);
+	// body.description = body.description.trim();
+
+
+	// body.id = todoNextId++;
+
+	// todos.push(body);
+
+
+
+	// res.json(body);
 
 });
 
-app.delete('/todos/:id', function (req, res){
+app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 
-	if (!matchedTodo){
-		res.status(404).json({"error" : "no todo found"});
+	if (!matchedTodo) {
+		res.status(404).json({
+			"error": "no todo found"
+		});
 	} else {
-		todos= _.without(todos, matchedTodo);
+		todos = _.without(todos, matchedTodo);
 		res.json(matchedTodo);
 	}
 });
 
 
-app.put('/todos/:id', function (req, res) {
+app.put('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.findWhere(todos, {
+		id: todoId
+	});
 	var body = _.pick(req.body, 'description', 'completed');
 	var validAttributes = {};
 
@@ -108,6 +130,9 @@ app.put('/todos/:id', function (req, res) {
 	res.json(matchedTodo);
 });
 
-app.listen(PORT, function(){
-	console.log('express is listening on port ' + PORT +'!');
+db.sequelize.sync().then(function() {
+	app.listen(PORT, function() {
+		console.log('express is listening on port ' + PORT + '!');
+	});
+
 });
